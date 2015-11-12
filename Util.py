eda6dbc6a73d98562101a3d1ea2cc690897fa3bc
jp94 <94jamespark@gmail.com>
2015-11-12 15:42:36 -0500

    Pushing headers for methods

--- Util.py
+++ Util.py
@@ -1,1 +1,26 @@
-#General Utility Methods for Algorithms
+# General Utility Methods for Algorithms
+import numpy as np
+
+
+def multiply_matrix(matrix_1, matrix_2):
+    return None
+
+
+def find_LU(matrix):
+    return None
+
+
+def find_QR(matrix):
+    return None
+
+
+def find_determinant(matrix):
+    return None
+
+
+def find_eigenvalues(matrix):
+    return None
+
+
+def find_eigenvectors(matrix):
+    return None
