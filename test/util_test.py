a35815211562e05799d5a3f64ee97c4046647f76
jp94 <jamespark@gatech.edu>
2015-11-13 18:26:55 -0500

    init commit for unit test file

---
+++ test/util_test.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+from Util import multiply_matrix
+
+
+class TestUtil(unittest.TestCase):
+    def test_multiply_matrix(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
