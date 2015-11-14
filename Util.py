d6f41795ef32abd03abbc8382554b1afd82a51be
jp94 <jamespark@gatech.edu>
2015-11-13 22:00:03 -0500

    implemented multiply_matrix method

--- Util.py
+++ Util.py
@@ -2,8 +2,23 @@
 import numpy as np
 
 
+# James
 def multiply_matrix(matrix_1, matrix_2):
-    return None
+    if matrix_1.shape[1] != matrix_2.shape[0]:
+        return -1
+
+    result = np.empty((matrix_1.shape[0], matrix_2.shape[1]))
+    # We can use transpose & dot product library function.
+    # Dot product of first rows of matrix_1 and matrix_2^t gives us first resulting number.of first row.
+    # Dot product of first row of matrix_1 and second row of matrix_2^t gives us second resulting number of first row.
+    matrix_2_t = matrix_2.transpose()
+    for i in range(matrix_1.shape[0]):
+        for j in range(matrix_2_t.shape[0]):
+            result[i, j] = matrix_1[i].dot(matrix_2_t[j])
+
+    print 'empty array:', result
+
+    return result
 
 
 def find_LU(matrix):
@@ -13,14 +28,17 @@ def find_LU(matrix):
 def find_QR(matrix):
     return None
 
-#Emeke
+
+# Emeke
 def find_determinant(matrix):
     return None
 
-#Emeke
+
+# Emeke
 def find_eigenvalues(matrix):
     return None
 
-#Emeke
+
+# Emeke
 def find_eigenvectors(matrix):
     return None
